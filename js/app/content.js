364bdc0531874f14103615ce58ac30e651a055dc
Yasser shaikh <yasser.s@media.net>
2016-11-06 21:44:31 +0530

    back to u ali.

--- js/app/content.js
+++ js/app/content.js
@@ -1,74 +1,79 @@
 $("body").prepend("\
-  <div class='wfit-container'>\
-    <div class='wfit-wrapper'>\
-        <div class='wfit-row wfit-header-row'>\
-          <span class='wfit-logo'><img src=''></span>\
-            <span class='wfit-header'>Style Inspector</span>\
-            <span id='end-session' class='wfit-end-btn'>End</span>\
+  <div class='wrap1'>\
+    <div class='wrap2'>\
+        <div class='head'>\
+          <span class='logo'><img src=''></span>\
+            <span class='title'>Style Inspector</span>\
+            <a class='close'>×</a>\
         </div>\
-        <div class='wfit-box'>\
-          <div class='wfit-title'>Live Inspector</div>\
-          <div class='wfit-row'>\
-            <span class='wfit-label wfit-half'>Family</span>\
-            <span class='wfit-label wfit-half'>Size</span>\
-            <span id='font-family-holder' class='wfit-half'>Arial</span>\
-            <span id='font-size-holder' class='wfit-half'>14px</span>\
-          </div>\
-          <div class='wfit-row'>\
-            <span class='wfit-label wfit-half'>Color</span>\
-            <span class='wfit-label wfit-half'>Background</span>\
-            <span class='wfit-half'>\
-              <span class='wfit-color-box wfit-boxes'>\
-                <span id='color-holder'>#dsada</span>\
-              </span>\
-            </span>\
-            <span class='wfit-half'>\
-              <span class='wfit-background-color-box wfit-boxes'>\
-                <span id='background-color-holder'>#dsada</span>\
-              </span>\
-            </span>\
-          </div>\
-        </div>\
-        <div class='wfit-box'>\
-          <div class='wfit-title'>Colors on this page</div>\
-          <div class='wfit-row wfit-color'>\
-            <span class='wfit-label wfit-half'>FONT COLORS</span>\
-            <span class='wfit-label wfit-half'>BG COLORS</span>\
-          </div>\
-          <hr>\
-          <div class='wfit-row wfit-row-scroll'>\
-            <div id='wfit-color-history'>\
-              <ul class='wfit-ul'></ul>\
-            </div>\
-          </div>\
-          <div class='wfit-row wfit-row-scroll'>\
-            <div id='wfit-background-history'>\
-              <ul class='wfit-ul'></ul>\
-            </div>\
-          </div>\
-        </div>\
-        <div class='wfit-box'>\
-          <div class='wfit-title'>Fonts on this page</div>\
-          <div id='wfit-font-history' class='wfit-row-scroll'>\
-            <ul class='wfit-ul'></ul>\
-          </div>\
-        </div>\
-        <div class='wfit-row'>\
-          <span class='wfit-credits'>Designed and Developed by <a href='http://yassershaikh.com' target='_blank'>Yasser Shaikh</a></span>\
+        <div class='main'>\
+	        <div class='box'>\
+	          <div class='label big-label'>Live Inspector</div>\
+	          <div class='row paddingtop10'>\
+	            <span class='label small-label width50percent'>Family</span>\
+	            <span class='label small-label width50percent'>Size</span>\
+	            <span class='width50percent yellow'>Arial</span>\
+	            <span class='width50percent yellow'>14px</span>\
+	          </div>\
+	          <div class='row paddingtop10'>\
+	            <span class='label small-label width50percent'>Color</span>\
+	            <span class='label small-label width50percent'>Background</span>\
+	            <span class='width50percent'>\
+	              <span class='colorbox wfit-boxes'>\
+	                <span id='color-holder'>#</span>\
+	              </span>\
+	            </span>\
+	            <span class='width50percent'>\
+	              <span class='colorbox wfit-boxes'>\
+	                <span id='background-color-holder'>#</span>\
+	              </span>\
+	            </span>\
+	          </div>\
+	        </div>\
+	        <div class='box row'>\
+	          <div class='label big-label'>Colors on this page</div>\
+	          <div class='wfit-row wfit-color'>\
+	            <span class='label small-label width50percent'>FONT COLORS</span>\
+	            <span class='label small-label width50percent'>BG COLORS</span>\
+	          </div>\
+	          <hr>\
+	          <div class='wfit-row wfit-row-scroll'>\
+	            <div id='wfit-color-history'>\
+	              <ul class='wfit-ul'></ul>\
+	            </div>\
+	          </div>\
+	          <div class='wfit-row wfit-row-scroll'>\
+	            <div id='wfit-background-history'>\
+	              <ul class='wfit-ul'></ul>\
+	            </div>\
+	          </div>\
+	        </div>\
+	        <div class='box paddingtop10 row'>\
+	          <div class='title label big-label'>Fonts on this page</div>\
+	          <div id='wfit-font-history' class='wfit-row-scroll'>\
+	            <ul class='wfit-ul'></ul>\
+	          </div>\
+	        </div>\
+	        <div class='footer'>\
+	          <span class='wfit-credits'>Designed by <a href='#' target='_blank'>Omar Faizan</a></span>\
+	          <span class='wfit-credits'>Developed by <a href='#' target='_blank'>Ali Rizvi & Yasser Shaikh</a></span>\
+	        </div>\
         </div>\
       </div>\
   </div>\
   "); 
- 
+
+$('head').append('<link href="https://fonts.googleapis.com/css?family=Raleway|Titillium+Web" rel="stylesheet">')
+
 var url = chrome.extension.getURL('icons/logo.png'); 
-$(".wfit-logo img").attr("src", url); 
+$(".logo img").attr("src", url); 
  
 var fontHistory = []; 
 var fontcolorHistory = []; 
 var backgroundcolorHistory = []; 
  
-$(".wfit-end-btn").click(function(){ 
-  $(".wfit-container").remove(); 
+$(".close").click(function(){ 
+  $(".wrap1").remove(); 
   $(".wfit-history").remove(); 
 });
 
@@ -112,7 +117,7 @@ $('*').hover(function(e) {
     currentFontColor = rgbToHex(currentFontColor); 
     $("#color-holder").html(currentFontColor); 
     $("#color-holder").css("color", fontColor(currentFontColor)); 
-    $(".wfit-color-box").css("background-color", currentFontColor); 
+    $(".colorbox").css("background-color", currentFontColor); 
      
     var currentBackgroundColor = hoverElem.css("background-color"); 
     currentBackgroundColor = rgbToHex(currentBackgroundColor); 
@@ -123,17 +128,17 @@ $('*').hover(function(e) {
     // bottom tab 
     if(fontHistory.indexOf(currentFont) == -1){ 
       fontHistory.push(currentFont); 
-      $(".wfit-wrapper").find('#wfit-font-history ul').append("<li>" + currentFont + "</li>"); 
+      $(".wrap2").find('#wfit-font-history ul').append("<li>" + currentFont + "</li>"); 
     } 
  
     if(fontcolorHistory.indexOf(currentFontColor) == -1){ 
       fontcolorHistory.push(currentFontColor); 
-      $(".wfit-wrapper").find('#wfit-color-history ul').append("<li>" + "<span class='wfit-boxes' style='background-color:"  + currentFontColor + "; color:" + fontColor(currentFontColor) + ";'>" + currentFontColor + "</span></li>"); 
+      $(".wrap2").find('#wfit-color-history ul').append("<li>" + "<span class='wfit-boxes' style='background-color:"  + currentFontColor + "; color:" + fontColor(currentFontColor) + ";'>" + currentFontColor + "</span></li>"); 
     } 
  
     if(backgroundcolorHistory.indexOf(currentBackgroundColor) == -1){ 
       backgroundcolorHistory.push(currentBackgroundColor); 
-      $(".wfit-wrapper").find('#wfit-background-history ul').append("<li>" + "<span class='wfit-boxes' style='background-color:"  + currentBackgroundColor + "; color:" + fontColor(currentBackgroundColor) + ";'>"  +  currentBackgroundColor + "</span></li>"); 
+      $(".wrap2").find('#wfit-background-history ul').append("<li>" + "<span class='wfit-boxes' style='background-color:"  + currentBackgroundColor + "; color:" + fontColor(currentBackgroundColor) + ";'>"  +  currentBackgroundColor + "</span></li>"); 
     } 
      
 }); 
